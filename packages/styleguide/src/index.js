250f42fec354d1e965d1930d292b774d99d53625
DavidSunny <uuunet1@gmail.com>
2018-04-28 20:00:33 +0900

    Add react styleguide

---
+++ packages/styleguide/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Catalog, pageLoader } from 'catalog';
+import Another from 'components/lib/Another';
+import Hello from 'components/lib/Hello';
+ReactDOM.render(
+  <Catalog
+    title='Catalog'
+    pages={[
+      {
+        imports: { Another, Hello },
+        path: '/',
+        title: 'Introduction',
+        content: pageLoader('intro.md'),
+      },
+    ]}
+  />,
+  document.getElementById('root')
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Catalog, pageLoader } from 'catalog';
+//
+// ReactDOM.render(
+//   <Catalog
+//     title='Catalog'
+//     pages={[
+//     {
+//       path: '/',
+//       title: 'Introduction',
+//       content: pageLoader('intro.md'),
+//     },
+//   ]}/>,
+//   document.getElementById('root')
+// );
